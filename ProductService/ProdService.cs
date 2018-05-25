085d196e2fb333ca1d165a3d4100b5dbd13863d0
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-05-25 22:10:53 +0800

    product service

---
+++ ProductService/ProdService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService
+{
+    public partial class ProdService
+    {
+        private readonly CommerceObjectContext objectContext;
+
+        public ProdService()
+        {
+            this.objectContext = new CommerceObjectContext();
+        }
+
+        public IEnumerable<Product> GetFeaturedProducts(
+        bool isCustomerPreferred)
+        {
+            var discount = isCustomerPreferred ? .95m : 1;
+            var products = (from p in this.objectContext
+            .Products
+                            where p.IsFeatured
+                            select p).AsEnumerable();
+            return from p in products
+                   select new Product
+                   {
+                       ProductId = p.ProductId,
+                       Name = p.Name,
+                       Description = p.Description,
+                       IsFeatured = p.IsFeatured,
+                       UnitPrice = p.UnitPrice * discount
+                   };
+        }
+    }
+}
