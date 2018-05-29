da7e89ae31085f6721e58534d32eb3b9e9883f31
Rasika Gayan Gunarathna <rasikagayan90@gmail.com>
2018-05-30 01:00:04 +0800

    Castle Windsor Simple Application

---
+++ CastleWindsorTutorials/CompositionRoot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleWindsorTutorials
+{
+    public class CompositionRoot : ICompositionRoot
+    {
+        readonly IConsoleWriter consoleWriter;
+
+        public void LogMessage(string message)
+        {
+            consoleWriter.LogMessage(message);
+        }
+
+        public CompositionRoot(IConsoleWriter consoleWriter)
+        {
+            this.consoleWriter = consoleWriter;
+            consoleWriter.LogMessage("Hello from CompositionRoot Constructor!");
+        }
+
+    }
+}
---
+++ CastleWindsorTutorials/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+
+namespace CastleWindsorTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+            container.Register(Component.For<ICompositionRoot>().
+                                         ImplementedBy<CompositionRoot>());
+            container.Register(Component.For<IConsoleWriter>().
+                                         ImplementedBy<ConsoleWriter>());
+            var root = container.Resolve<ICompositionRoot>();
+
+            root.LogMessage("This is simple message");
+            Console.ReadLine();
+        }
+    }       
+}
